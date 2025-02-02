042717a109b3c4901219e0ab04e72f3cfc67b8c8
srinukesari <srinukesari333@gmail.com>
2025-02-01 22:52:27 -0500

    added flask changes to expose endpoint in localhost and response structure changed

--- models/ServiceResponse.py
+++ models/ServiceResponse.py
@@ -1,4 +1,5 @@
 from models.constants import LOW, MEDIUM, HIGH
+import json
 
 class ServiceResponse:
     def __init__(self):
@@ -7,6 +8,8 @@ class ServiceResponse:
         self.operationalCosts = 0
         self.damageCosts = 0
         self.severityReport = {LOW: 0, MEDIUM: 0, HIGH: 0}
+        self.severityCoordinates = {True:{LOW:[], MEDIUM:[], HIGH:[]},
+                                    False:{LOW:[], MEDIUM:[], HIGH:[]}}
 
     def address_fire(self):
         self.firesAddressed += 1
@@ -25,15 +28,17 @@ class ServiceResponse:
         else:
             self.damageCosts += 50000
 
-    def add_incident(self, resource_cost, sev, resolved):
+    def add_incident(self, resource_cost, sev, resolved,coordinates):
         self.severityReport[sev] += 1
 
         if not resolved:
             self.add_damage_cost(sev)
             self.delay_fire()
+            self.severityCoordinates[False][sev].append(coordinates)
         else:
             self.address_fire()
             self.add_operational_cost(resource_cost)
+            self.severityCoordinates[True][sev].append(coordinates)
 
     def get_report(self):
         """
@@ -44,11 +49,14 @@ class ServiceResponse:
             - Estimated damage costs from delayed responses
             - Fire severity report
             """
-        report = (
-            f"Number of fires addressed: {self.firesAddressed}\n"
-            f"Number of fires delayed: {self.firesDelayed}\n"
-            f"Total operational costs: ${self.operationalCosts}\n"
-            f"Estimated damage costs from delayed responses: ${self.damageCosts}\n"
-            f"Fire severity report: {self.severityReport}"
-        )
-        return report
+        report_data = {
+            "Number_of_fires_addressed": self.firesAddressed,
+            "Number_of_fires_delayed": self.firesDelayed,
+            "Total_operational_costs": self.operationalCosts,
+            "Estimated_damage_costs_from_delayed_responses": self.damageCosts,
+            "Fire_severity_report": self.severityReport,
+            "Coordinates": self.severityCoordinates
+        }
+
+        # report_json = json.dumps(report_data)
+        return report_data
